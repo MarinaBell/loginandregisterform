732dfc6e8a48509c7695c10a2fc0365293631693
Marina Belenkaya <marina_vorobyeva1423@mail.ru>
2021-11-26 12:07:21 +0400

    feat:add files

---
+++ script.js
@@ -0,0 +1,27 @@
+const loginButton = document.querySelector('.login-button');
+const registerButton = document.querySelector('.register-button');
+const loginForm = document.querySelector('.login-form');
+const registerForm = document.querySelector('.register-form');
+
+registerButton.addEventListener('click', function(){
+   loginForm.remove('active');
+   registerForm.add('active');
+
+   //  loginButton.remove('active');
+   
+})
+
+registerButton.addEventListener('click', function(){
+  
+    loginButton.remove('active');
+   
+})
+
+
+loginButton.addEventListener('click', function(){
+   loginForm.add('active');
+      registerForm.remove('active');
+   
+})
+
+
